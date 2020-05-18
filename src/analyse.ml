rems-project/read-dwarf

commit c0753b2e81c99ab80b397cbfece22ea12a0189f8

flip file and subprogram_name in ppy

src/analyse.ml
@@ -1,4 +1,4 @@
-open Logs.Logger (struct
+sopen Logs.Logger (struct
   let str = __MODULE__
 end)
 
@@ -774,11 +774,11 @@ let pp_dwarf_source_file_lines' (ds : Dwarf.dwarf_static) (pp_actual_line : bool
         | Some s -> Some (Filename.concat comp_dir'' s)
       )
   in
-  file ^ ":"
+  subprogram_name ^ ":"
   ^ Nat_big_num.to_string lnr.lnr_line
   ^ "."
   ^ Nat_big_num.to_string lnr.lnr_column
-  ^ " (" ^ subprogram_name ^ ")"
+  ^ " (" ^ file ^ ")"
   (*  ^ (if elifi.elifi_start then "S" else "s")*)
   ^ (if lnr.lnr_is_stmt then "S" else "s")
   ^ (if lnr.lnr_basic_block then "B" else "b")

commit 5aa3d244b40c8b4ed6ce2762d53a64d482e23007

Fix compile error introduced by commit "c0753b2"
The previous compiling commit was:
08d017cd2b17f5ad01d81adb5c847277d534398d

The only code-changing commit in between was the faulty commit:
c0753b2e81c99ab80b397cbfece22ea12a0189f8

src/analyse.ml
@@ -1,4 +1,4 @@
-sopen Logs.Logger (struct
+open Logs.Logger (struct
   let str = __MODULE__
 end)
 