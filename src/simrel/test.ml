7b9f93e7d1a1dda52480e5063bb046c8bfe00906
Dhruv Makwana <dcm41@cl.cam.ac.uk>
2020-11-05 14:53:10 +1300

    Add simrel checking for small function (test2)

---
+++ src/simrel/test.ml
@@ -0,0 +1,28 @@
+(** Temporary exe to test simrel code. *)
+
+open Logs.Logger (struct
+  let str = __MODULE__
+end)
+
+open Simrel.Base
+
+let o0_elf = "vm-stuff/pkvm-O0/el2.elf"
+
+let o2_elf = "vm-stuff/pkvm-O2/el2.elf"
+
+let tree_pair name : tree pair =
+  {
+    o0 = Run.Func.get_state_tree ~elf:o0_elf ~name ();
+    o2 = Run.Func.get_state_tree ~elf:o2_elf ~name ();
+  }
+
+let main () =
+  let test2 = tree_pair "hyp_get_page_tv_test2" in
+  let test2_matched = match_up test2 in
+  base "%s" (if test2_related test2_matched then "SAME" else "DIFF");
+  ignore test2_matched
+
+let () =
+  Config.CommonOpt.quick_exe ~name:"simrel-test"
+    ~doc:"A small harness for running simulation relation checking code on examples."
+    main
--- src/state/base.mli
+++ src/state/base.mli
@@ -227,6 +227,7 @@ type tval = Tval.t
     Implementation detail: This type has an imperative interface even if the
     underlying {!SymbolicFragment} has a pure interface.*)
 module Mem : sig
+
   type t
 
   (** Add a new fragment with the specified base *)
