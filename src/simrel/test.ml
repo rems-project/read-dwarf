ab5ba57dd57cdd28bef83a1b6e732fb002a2f85c
Dhruv Makwana <dcm41@cl.cam.ac.uk>
2020-12-04 09:35:29 +1300

    Clean up code

--- src/simrel/test.ml
+++ src/simrel/test.ml
@@ -17,12 +17,12 @@ let tree_pair name : tree pair =
   }
 
 let main () =
-  (* let test2 = tree_pair "hyp_get_page_tv_test2" in *)
-  (* let test2_matched = Test2.match_up test2 in *)
-  (* base "test2: %s" (if Test2.related test2_matched then "SAME" else "DIFF") *)
+  let test2 = tree_pair "hyp_get_page_tv_test2" in
+  let test2_matched = Test2.infer_rels test2 in
+  base "test2: %s" (if check_rel test2_matched then "SAME" else "DIFF");
   let test3 = tree_pair "hyp_get_page_tv_test3" in
-  let test3_matched = Test3.match_up test3 in
-  base "test3: %s" (if Test3.related test3_matched then "SAME" else "DIFF")
+  let test3_matched = Test3.infer_rels test3 in
+  base "test3: %s" (if check_rel test3_matched then "SAME" else "DIFF")
 
 let () =
   Config.CommonOpt.quick_exe ~name:"simrel-test"
