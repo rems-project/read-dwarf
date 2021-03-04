33fb6981ca783fe41d558bc214dfc3beedca7e6d
Dhruv Makwana <dcm41@cl.cam.ac.uk>
2021-03-04 13:46:10 +1300

    Add simrel test exe target to Makefile

--- src/simrel/run_examples.sh
+++ src/simrel/run_examples.sh
@@ -2,29 +2,10 @@
 
 set -euo pipefail
 
-LINUX_DIR="../linux"
-KVM_DIR="arch/arm64/kvm/hyp/nvhe"
-REL_ELF_PATH="$KVM_DIR/el2.o"
-MAKEFILE="$LINUX_DIR/$KVM_DIR/Makefile"
 function target() {
     LEVEL="$1"
     echo "src/simrel/O$LEVEL.elf"
 }
-
-function copy_elf() {
-    LEVEl="$1"
-    sed -i "s/-O[[:digit:]]/-O$LEVEL/g" "$MAKEFILE"
-    make -C "$LINUX_DIR" \
-        ARCH=arm64 \
-        CC=../linux-x86/clang-r383902/bin/clang \
-        CROSS_COMPILE=aarch64-linux-gnu- \
-        -j $(nproc) \
-        "$REL_ELF_PATH"
-    TARGET="$(target $LEVEL)"
-    cp -a "$LINUX_DIR/$REL_ELF_PATH" "$TARGET"
-    aarch64-linux-gnu-objdump -DS "$TARGET" > "$TARGET.objdump-DS"
-}
-
 
 EXE="./read-dwarf"
 CMD="run-func"
@@ -39,7 +20,6 @@ function save_state_tree_of_to() {
 
 function run_examples_level() {
     LEVEL=$1
-    copy_elf "$LEVEL"
     save_state_tree_of_to "$LEVEL" hyp_get_page_tv_test2 "./src/simrel/O${LEVEL}_test2.state"
     save_state_tree_of_to "$LEVEL" hyp_get_page_tv_test3 "./src/simrel/O${LEVEL}_test3.state"
 #   save_state_tree_of_to "$LEVEL" hyp_get_page_tv_test "./src/simrel/O${LEVEL}_test4.state"
