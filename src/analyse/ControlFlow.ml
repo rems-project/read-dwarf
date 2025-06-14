11557319a2044cf76b91a29303c3714cf2c69451
Peter Sewell <Peter.Sewell@cl.cam.ac.uk>
2025-06-14 10:49:08 +0100

    fix syntax for parsing objdump with %-separated relocations

--- src/analyse/ControlFlow.ml
+++ src/analyse/ControlFlow.ml
@@ -451,10 +451,19 @@ AArch64:
    10004:	52800129 	mov	w9, #0x9                   	// #9
  *)
 
+(* matej version *)
+(*
 let relocation_regexp_string = "[ \t][0-9a-fA-F]+:[ \t]\\([0-9A-Z_]+\\)\t\\(.*\\)"
 
 let objdump_line_regexp =
   Str.regexp (" *\\([0-9a-fA-F]+\\):[ \t]\\([0-9a-fA-F ]+\\)\t\\([^ \r\t\n]+\\)[ \t]*\\([^:]*\\)\\(" ^ relocation_regexp_string ^ "\\)?$")
+*)
+(* ps version *)
+
+let relocation_regexp_string = "%[ \t]+[0-9a-fA-F]+:[ \t]+\\([0-9A-Z_]+\\)[ \t]+\\(.*\\)"
+
+let objdump_line_regexp =
+  Str.regexp (" *\\([0-9a-fA-F]+\\):[ \t]\\([0-9a-fA-F ]+\\)\t\\([^ \r\t\n]+\\)[ \t]*\\([^%]*\\)\\(" ^ relocation_regexp_string ^ "\\)?$")
 
 let section_start_line_regexp =
   Str.regexp "Disassembly of section \\(.*\\):$"
